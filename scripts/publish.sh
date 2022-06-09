32b471a71ed19ddc004ca15fd1d724b7706c1600
Rob Hilgefort <rjhilgefort@gmail.com>
2022-06-09 17:44:24 -0600

    testing changeset publishes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,6 @@
 yarn build:changed # Executes TypeScript and allows lint to work properly
 yarn lint:changed
 yarn test:changed
+changeset version
 changeset publish
 git push --follow-tags
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+changeset version
 changeset publish
 git push --follow-tags
