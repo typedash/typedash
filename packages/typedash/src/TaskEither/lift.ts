6dd458e87c905930a5715c3ddce464f4293b5dee
Josh York <joshua.s.york@gmail.com>
2023-02-23 18:35:37 -0700

    add TE.lift example

--- packages/typedash/src/TaskEither/lift.ts
+++ packages/typedash/src/TaskEither/lift.ts
@@ -5,6 +5,16 @@ const defaultOnRejected = (reason: unknown) =>
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
   newError(`An error was encountered. ${reason}`)
 
+/**
+ * @description Lifts a promise returning function into a TE
+ * @example
+ *  const foo = () => Promise.resolve('foo')
+ *  const fooTE = lift((error) => new Error(`it broke!`))(foo)
+ *  pipe(
+ *    fooTE(),
+ *    TE.map(x => x), // x = 'foo'
+ *  )
+ */
 export const lift =
   <
     ProvidedOnRejected extends (error: unknown) => unknown,
